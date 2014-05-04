a8deb5965c61c626a508c32b712be63689e5112e
Jose Trezza <josetrezza@gmail.com>
2014-05-04 13:50:01 -0400

    database

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'gaia',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'ETPwTAUEd5',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
